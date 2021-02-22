cf08b58a719e1b2c0b4a5ea2a91a022e939ebbba
Alejandro Brito <69440796+alejandro-brito@users.noreply.github.com>
2021-02-21 22:11:45 -0500

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,12 @@
+import https from 'https'
 import axios from 'axios'
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/',
     hostname: process.env.REACT_APP_API_HOST || 'http://localhost:3000/',
+    httpsAgent: https.Agent({
+        rejectUnauthorized: false,
+    }),
 });
 
 export const addBook = payload => api.post(`/admin/book/create`, payload)
